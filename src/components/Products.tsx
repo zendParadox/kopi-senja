18a14dd46c76c4c22ed27b622f91e0eff739cc38
zendParadox <raflirama7446@gmail.com>
2025-09-29 08:38:56 +0700

    add Products component to display coffee selections with images and pricing

---
+++ src/components/Products.tsx
@@ -0,0 +1,86 @@
+import Image from "next/image";
+
+const products = [
+  {
+    id: 1,
+    name: "Harmoni Senja Blend",
+    href: "#",
+    imageSrc: "/images/products/produk1.png",
+    imageAlt: "Front of men's Basic Tee in black.",
+    price: "85.000",
+    color: "Dark Chocolate, Brown Sugar.",
+  },
+  {
+    id: 2,
+    name: "Aroma Pulau Dewata",
+    href: "#",
+    imageSrc: "/images/products/produk2.png",
+    imageAlt: "Front of men's Basic Tee in white.",
+    price: "75.000",
+    color: "Orange Peel, Lemongrass, Floral.",
+  },
+  {
+    id: 3,
+    name: "Senja Klasik Gayo",
+    href: "#",
+    imageSrc: "/images/products/produk3.png",
+    imageAlt: "Front of men's Basic Tee in dark gray.",
+    price: "81.000",
+    color: "Roasted Almond, Caramel, Full Body.",
+  },
+  {
+    id: 4,
+    name: "Malabar Puspita",
+    href: "#",
+    imageSrc: "/images/products/produk4.png",
+    imageAlt:
+      "Front of men's Artwork Tee in peach with white and brown dots forming an isometric cube.",
+    price: "73.000",
+    color: "Red Wine, Black Cherry.",
+  },
+];
+
+export default function Products() {
+  return (
+    <div className="bg-slate-50 py-20 mb-96">
+      <div className="mx-auto max-w-2xl px-4  sm:px-6  lg:max-w-7xl lg:px-8">
+        <h2 className="text-2xl font-bold tracking-tight text-gray-900">
+          Pilihan Biji Kopi Terbaik Kami
+        </h2>
+
+        <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+          {products.map((product) => (
+            <div key={product.id} className="group relative">
+              <Image
+                src={product.imageSrc}
+                alt={product.imageAlt}
+                className="aspect-square w-full rounded-md bg-gray-200 object-cover group-hover:opacity-75 lg:aspect-auto lg:h-80"
+                width={500}
+                height={500}
+              />
+              {/* <img
+                alt={product.imageAlt}
+                src={product.imageSrc}
+                className="aspect-square w-full rounded-md bg-gray-200 object-cover group-hover:opacity-75 lg:aspect-auto lg:h-80"
+              /> */}
+              <div className="mt-4 flex justify-between">
+                <div>
+                  <h3 className="text-sm text-gray-700">
+                    <a href={product.href}>
+                      <span aria-hidden="true" className="absolute inset-0" />
+                      {product.name}
+                    </a>
+                  </h3>
+                  <p className="mt-1 text-sm text-gray-500">{product.color}</p>
+                </div>
+                <p className="text-sm font-medium text-gray-900">
+                  {product.price}
+                </p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
